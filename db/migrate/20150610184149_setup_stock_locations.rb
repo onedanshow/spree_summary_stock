aa87758c5e12fd28eb9fb62643b005c82c68d1b9
Daniel Dixon <code@danieldixon.com>
2015-06-17 12:09:56 -0500

    Updated migration.

--- db/migrate/20150610184149_setup_stock_locations.rb
+++ db/migrate/20150610184149_setup_stock_locations.rb
@@ -3,8 +3,11 @@ class SetupStockLocations < ActiveRecord::Migration
     # make the non-summational stock locations
     Spree::StockLocation.update_all(active:false)
     # make the summational stock location
-    Spree::StockLocation.create!(default: true, active:true, name: "Stock Summary")
+    sl = Spree::StockLocation.create!(default: true, active:true, name: "Stock Summary")
     # update stock levels for summational stock location
     Spree::StockLocation.not_summational.first.stock_items.each{|si| si.send(:summarize_stock) }
+    # move all pending shipments to use the new stock location
+    Spree::Shipment.pending.update_all( stock_location_id:sl.id )
+    Spree::Shipment.ready.update_all( stock_location_id:sl.id )
   end
 end
