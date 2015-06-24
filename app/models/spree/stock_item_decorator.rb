2062d918eb31d761c87cab19df10a84ef25215a0
Daniel Dixon <code@danieldixon.com>
2015-06-24 15:18:19 -0500

    Avoid creation loop when making a new variant.

--- app/models/spree/stock_item_decorator.rb
+++ app/models/spree/stock_item_decorator.rb
@@ -4,14 +4,14 @@ Spree::StockItem.class_eval do
 
   private
 
-    def summarize_stock
+    def summarize_stock      
       # DD: ignore stock_location of current stock item to prevent infinite loop
       virtual_warehouse = Spree::StockLocation.summational.where.not(id:stock_location.try(:id)).first 
 
       if self.variant && virtual_warehouse
         real_warehouses = Spree::StockLocation.not_summational.where.not(id:virtual_warehouse.try(:id))
         sum = real_warehouses.inject(0) {|sum,w| sum + w.count_on_hand(variant).to_i }
-        virtual_warehouse.set_up_stock_item(self.variant).set_count_on_hand(sum)
+        virtual_warehouse.stock_item(self.variant).try(:set_count_on_hand,sum)
       end
     end
 
